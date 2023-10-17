67b8e93f6387e86b3d0220eef7509234b3835db5
فرسان القباطي <129460913+forsanfar@users.noreply.github.com>
2023-10-17 16:26:54 +0300

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -88,7 +88,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="tel:+967776562483"
+                  href="tel:+967779562483"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
